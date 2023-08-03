7d78c46fe656d0904c11e998e1013842a893ad44
Kurrry <ajohnsond11@gmail.com>
2023-08-02 21:47:16 -0600

    testing stuff

--- src/main/AppDriver.java
+++ src/main/AppDriver.java
@@ -6,7 +6,7 @@ import tracker.WordTracker;
 public class AppDriver {
     public static void main(String[] args) {
         String[] test = new String[2];
-        test[0] = "src/res/test1.txt";
+        test[0] = "src/res/test2.txt"; // change number for testing
         test[1] = "-pf";
         new WordTracker(test);
     }
